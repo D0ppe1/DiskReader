ade214ab3ab2c0c2368816cf88ffc34b0ef5c771
Andrei <an.kolykhavlo@mail.ru>
2023-08-03 22:44:11 +0300

    Created a function that return a list of user's disks.

--- disk_list.py
+++ disk_list.py
@@ -1,50 +1,22 @@
-# import os
-# import sys
-
 import psutil
 
-# В ф-ции нужно, чтобы мы определили:
-# 1. Кол-во кортежей done
-# 2. Нужно создать кол-во переменных равное кол-ву кортежей
-# 3. Сделать из переменных список
-# 4. Вывести нулевой индекс каждого списка
 
-
-# function retrun a list of disks
 def disk_list():
-    # а - список с кортежами
-    a = psutil.disk_partitions(all=False)
-    lengtha = len(a)
-    print(lengtha)
-
-    print(a[0])
-    # b - переменная которая берет 1-й кортеж
-    b = a[0]
-    # print(b)
-    # превращаем b в список b
-    b = list(b)
-    # print(b)
-    # возвращаем 0-вой индекс списка b
-    return b[0]
-
-print(disk_list())
-
+    # function that retrun a list of disks
+    devices = [] # list only with name of user's device(disks)
+    disk = []
+    disklist = psutil.disk_partitions(all=False)  # list with user data(How many disks does he have)
+    for disk in disklist:
+        # print(disks.device)
+        devices.append(disk.device)
+    return devices # return the list with disks we need
+    # Seconde variaton of code
+    # i = 0
+    # while i<len(a):
+    #     # print(a[i])
+    #     c = a[i]
+    #     b.append(c.device) # тут на каждой итерации добавляем в список нужный нам объект
+    #     i += 1
 
 
-# def disk_list():
-    # templ = "%-17s"
-    # print(templ % ('Disks:'))
-    # for part in psutil.disk_partitions(all=False):
-    #     if os.name == "nt":
-    #         if 'cdrom' in part.opts or part.fstype == '':
-    #             continue
-    #     print(templ % (part.device))
-
-
-# if __name__ == '__disk_list__':
-#     sys.exit(disk_list())
-
-
-
-# a = psutil.disk_partitions()
-# print(a)
+# print(disk_list())
