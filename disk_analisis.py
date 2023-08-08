55b2d286c33090f27f5efeaec194c4e264858357
Andrei <an.kolykhavlo@mail.ru>
2023-08-08 19:24:10 +0300

    Created a function that return information about   user's disk.

--- disk_analisis.py
+++ disk_analisis.py
@@ -1,4 +1,22 @@
 import psutil
+from psutil._common import bytes2human
 
-b = psutil.disk_usage('/')
-print(b)
+
+def disk_analisis(disk: str):
+    # Function that return information about a disk that user indicate
+    total, used, free, percent = psutil.disk_usage(f'{disk}:/')
+    # I guess that "total,used,free,percent" are parametrs of psutil.disk_usage
+    # which help to get info about a disk
+    for info in disk:
+        print(
+            f"Всего -- {bytes2human(total)} -- места на диске\n"
+            f"Использованно -- {bytes2human(used)} -- места на диске\n"
+            f"Свободно -- {bytes2human(free)} -- места на диске\n"
+            f"Процент заполнения диска -- {percent}% --\n"
+        )
+
+    return info
+
+
+print(disk_analisis("d"))  # In parantheses indicated a name of disk
+
--- disk_list.py
+++ disk_list.py
@@ -3,13 +3,13 @@ import psutil
 
 def disk_list():
     # function that retrun a list of disks
-    devices = [] # list only with name of user's device(disks)
+    devices = [] # list only with name of user's devices(disks)
     disk = []
     disklist = psutil.disk_partitions(all=False)  # list with user data(How many disks does he have)
     for disk in disklist:
         # print(disks.device)
         devices.append(disk.device)
-    return devices # return the list with disks we need
+    return print(f"Список дисков:{devices}") # return the list with disks we need
     # Seconde variaton of code
     # i = 0
     # while i<len(a):
@@ -19,4 +19,4 @@ def disk_list():
     #     i += 1
 
 
-# print(disk_list())
+print(disk_list())
