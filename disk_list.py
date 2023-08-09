9d102ae13d0f73717cccfe57bc16d5edb0f3f778
Andrei <an.kolykhavlo@mail.ru>
2023-08-09 21:04:53 +0300

    1. In disk_list changed return value (deleted func "print" from return). 2. In main added opportunity to see a list of user's disks.

--- disk_list.py
+++ disk_list.py
@@ -3,13 +3,13 @@ import psutil
 
 def disk_list():
     # function that retrun a list of disks
-    devices = [] # list only with name of user's devices(disks)
+    devices = []  # list only with name of user's devices(disks)
     disk = []
     disklist = psutil.disk_partitions(all=False)  # list with user data(How many disks does he have)
     for disk in disklist:
         # print(disks.device)
         devices.append(disk.device)
-    return print(f"Список дисков:{devices}") # return the list with disks we need
+    return devices  # return the list with disks we need
     # Seconde variaton of code
     # i = 0
     # while i<len(a):
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import *
-
+from disk_list import *
+from disk_analisis import *
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-    return '<h1>Hello, world!<h1>'
+    return f"Вот список ваших дисков: {disk_list()}"
 
 
+# disk_list(), disk_analisis()
 if __name__ == '__main__':
     app.run()
