cb6853097282d0fa27140cf9f3af3a8b3908b571
Andrei <an.kolykhavlo@mail.ru>
2023-08-10 21:17:35 +0300

    Deleted cycle from disk_analisis. There is a problem with indent in "stats".

--- disk_analisis.py
+++ disk_analisis.py
@@ -7,16 +7,12 @@ def disk_analisis(disk: str):
     total, used, free, percent = psutil.disk_usage(f'{disk}:/')
     # I guess that "total,used,free,percent" are parametrs of psutil.disk_usage
     # which help to get info about a disk
-    for info in disk:
-        print(
-            f"Всего -- {bytes2human(total)} -- места на диске\n"
-            f"Использованно -- {bytes2human(used)} -- места на диске\n"
-            f"Свободно -- {bytes2human(free)} -- места на диске\n"
-            f"Процент заполнения диска -- {percent}% --\n"
-        )
 
-    return info
+    stats = f"Всего -- {bytes2human(total)} -- места на диске "\
+            f"Использованно -- {bytes2human(used)} -- места на диске "\
+            f"Свободно -- {bytes2human(free)} -- места на диске "\
+            f"Процент заполнения диска -- {int(percent)}% -- "\
 
+    return stats
 
-print(disk_analisis("d"))  # In parantheses indicated a name of disk
-
+print(disk_analisis("c"))  # In parantheses indicated a name of disk
